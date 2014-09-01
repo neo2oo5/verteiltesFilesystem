682db936cade2a3275a48abc7bbb260964f1ced0
Kevin <>
2014-09-01 17:26:39 +0200

    fileSystemException und grobe definition der fileSystem

--- verteiltesFilesystem_origin/src/substructure/fileSystem.java
+++ verteiltesFilesystem_origin/src/substructure/fileSystem.java
@@ -6,15 +6,28 @@
 
 package substructure;
 
+import java.io.File;
+
 /**
  *
  * @author xoxoxo
  */
 public class fileSystem {
     
+    private File[] fileSystem;
+    private boolean isLocked        = false;
+    private boolean isInterrupted   = false;
+    private String  whoLocked;
+    private String  token;
+   
+    
     private fileSystem() {
     }
     
+    /**
+     * Gibt die Instanz zurueck (Singelton)
+     * @return
+     */
     public static fileSystem getInstance() {
         return fileSystemHolder.INSTANCE;
     }
@@ -22,5 +35,78 @@ public class fileSystem {
     private static class fileSystemHolder {
 
         private static final fileSystem INSTANCE = new fileSystem();
+    }
+
+    /**
+     *  sperrt Struktur fuer andere Module
+     *  Jedes Modul generiert einen Token somit werden doppel zugriffe verhindert
+     * @param token
+     */
+    public void setLock(String token)
+    {
+        isLocked = true;
+        this.token = token;
+    }
+    
+    /**
+     * Entfernt den Lock
+     * @param token
+     * @return
+     */
+    public boolean removeLock(String token)
+    {
+        if(token.equals(this.token))
+        {
+            isLocked = true;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+ 
+    /**
+     * Sollte ein Modul vergessen den Lock entfernen kann er auch so entfernt werden
+     * Letzte Moeglickeit!
+     * Muss man sich noch einigen wann er benutzt werden darf
+     * @return
+     */
+    public boolean rootremoveLock()
+    {
+        this.token  ="";
+        isLocked    = false;
+        return true;
+    }
+    
+    /**
+     * Muss  zwischen Langen operationen unterbrochen werden kann man diese funktion nutzen um zugriff zu erlangen
+     * @return
+     */
+    public boolean setInterrupt()
+    {
+        isInterrupted = true;
+        return true;
+    }
+            
+    /**
+     *  Gibt den Lock Status zurueck
+     * @return
+     */
+    public boolean getLockState()
+    {
+        return isLocked;
+    }
+    
+    /**
+     *  Fuegt file oder folder ein wirft exception falls interrupt angefordert wurde
+     * @throws fileSystemException
+     */
+    public void addObject() throws fileSystemException
+    {
+        if(isInterrupted == true)
+        {
+            throw new fileSystemException("Ein Interrupt wurde gemeldet!");
+        }
     }
 }
