b0e3fc1724d024f75a843d309f3159d6873d5ff9
No0b7 <davidlampa7@gmail.com>
2014-10-12 20:01:33 +0200

    Filetransfer throw entfernt - try catch eingefügt

--- verteiltesFilesystem_origin/src/network/FiletransferClient.java
+++ verteiltesFilesystem_origin/src/network/FiletransferClient.java
@@ -7,8 +7,11 @@ package network;
 
 import java.io.BufferedOutputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import substructure.GUIOutput;
 
@@ -21,21 +24,27 @@ public class FiletransferClient
 
     static GUIOutput out = GUIOutput.getInstance();
 
-    public static boolean FileTransferClient(String[] args) throws Exception
+    public static boolean FileTransferClient(String[] args)
     {
-        out.print("FileTransferClient startet", 1);
-        Socket sock = new Socket(args[0], 1718);
-        byte[] mybytearray = new byte[1024];
-        InputStream is = sock.getInputStream();
-        String path = args[2]+args[1];
-        String outputdatei = path;
-        FileOutputStream fos = new FileOutputStream(path);
-        BufferedOutputStream bos = new BufferedOutputStream(fos);
-        int bytesRead = is.read(mybytearray, 0, mybytearray.length);
-        bos.write(mybytearray, 0, bytesRead);
-        bos.close();
-        sock.close();
-        
+        try
+        {
+            out.print("FileTransferClient startet", 1);
+            Socket sock = new Socket(args[0], 1718);
+            byte[] mybytearray = new byte[1024];
+            InputStream is = sock.getInputStream();
+            String path = args[2] + args[1];
+            String outputdatei = path;
+            FileOutputStream fos = new FileOutputStream(path);
+            BufferedOutputStream bos = new BufferedOutputStream(fos);
+            int bytesRead = is.read(mybytearray, 0, mybytearray.length);
+            bos.write(mybytearray, 0, bytesRead);
+            bos.close();
+            sock.close();
+
+        } catch (IOException ex)
+        {
+            return false;
+        }
         return true;
     }
 }
