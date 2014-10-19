58d31eed6276324f06cc409ba2241676cc75a25e
No0b7 <davidlampa7@gmail.com>
2014-10-19 02:06:11 +0200

    as commit

--- verteiltesFilesystem_origin/src/gui/Explorer/RightClickMenu.java
+++ verteiltesFilesystem_origin/src/gui/Explorer/RightClickMenu.java
@@ -23,15 +23,12 @@ import network.*;
  *
  * @author Kevin Bonner <kevin.bonner@gmx.de>
  */
-public class RightClickMenu
-{
+public class RightClickMenu {
 
     private static String CFI_CMD = "createFile";
     private static String RFI_CMD = "removeFile";
     private static String DFI_CMD = "downloadFile";
     private static String REFI_CMD = "renameFile";
-
-    
 
     private JPopupMenu popup;
     private JMenuItem menuItemFileDownload, menuItemFileDelete, menuItemFileCreate, menuItemFileRename;
@@ -41,8 +38,7 @@ public class RightClickMenu
      *
      * @param e
      */
-    public RightClickMenu(MouseEvent e)
-    {
+    public RightClickMenu(MouseEvent e) {
         popup = new JPopupMenu();
 
         menuItemFileDownload = new JMenuItem("Datei downloaden");
@@ -60,48 +56,33 @@ public class RightClickMenu
         menuItemFileDelete.setActionCommand(RFI_CMD);
         menuItemFileRename.setActionCommand(REFI_CMD);
 
-        
-        
-        
         TreePath currentSelection = DynamicTree.getTree().getSelectionPath();
         String[] args = new String[5];
         DefaultMutableTreeNode currentNode = null;
 
-        if(currentSelection != null && args != null)
-        {
+        if (currentSelection != null) {
             currentNode = (DefaultMutableTreeNode) (currentSelection.getLastPathComponent());
-            if(currentNode != null)
-            {
+            if (currentNode != null) {
                 args = ExplorerHelper.getNetOperationData(currentNode);
-                
-                //eigene IP
-                if(Config.getCurrentIp().equals(args[0]))
-                {
-                    popup.add(menuItemFileCreate);
-                    popup.add(menuItemFileDelete);
-                    popup.add(menuItemFileRename);
+
+                if (args != null) {
+                    //eigene IP
+                    if (Config.getCurrentIp().equals(args[0])) {
+                        popup.add(menuItemFileCreate);
+                        popup.add(menuItemFileDelete);
+                        popup.add(menuItemFileRename);
+                    } else if (Interfaces.interfaceIAmAdmin()) {
+                        popup.add(menuItemFileDownload);
+                        popup.add(menuItemFileCreate);
+                        popup.add(menuItemFileDelete);
+                        popup.add(menuItemFileRename);
+                    } else if (Interfaces.interfaceIAmAdmin() == false && !Config.getCurrentIp().equals(args[0])) {
+                        popup.add(menuItemFileDownload);
+                    }
                 }
-                else if(Interfaces.interfaceIAmAdmin())
-                {
-                    popup.add(menuItemFileDownload);
-                    popup.add(menuItemFileCreate);
-                    popup.add(menuItemFileDelete);
-                    popup.add(menuItemFileRename);
-                }
-                else if(Interfaces.interfaceIAmAdmin() == false && !Config.getCurrentIp().equals(args[0]))
-                {
-                    popup.add(menuItemFileDownload);
-                }
-                
-                
-                
+
             }
         }
-
-
-
-
-        
 
         popup.show(e.getComponent(), e.getX(), e.getY());
     }
@@ -111,65 +92,42 @@ public class RightClickMenu
      *
      *
      */
-    class PopupListener implements ActionListener
-    {
+    class PopupListener implements ActionListener {
 
         @Override
-        public void actionPerformed(ActionEvent e)
-        {
+        public void actionPerformed(ActionEvent e) {
             String command = e.getActionCommand(); //To change body of generated methods, choose Tools | Templates.
             TreePath currentSelection = DynamicTree.getTree().getSelectionPath();
             String[] args = new String[5];
             DefaultMutableTreeNode currentNode = null;
-            
-            if(currentSelection != null)
-            {
+
+            if (currentSelection != null) {
                 currentNode = (DefaultMutableTreeNode) (currentSelection.getLastPathComponent());
                 args = ExplorerHelper.getNetOperationData(currentNode);
-                
-                for(String arg: args)
-                {
-                    System.out.print(arg+"\n");
-                }
+
             }
-            
-           
-            
-            
-            if(currentNode != null && currentSelection != null)
-            {
-                
-                System.out.print("selection: "+currentSelection + "node" + currentNode.getUserObject()+" \n");
-                System.out.print("admin: "+Interfaces.interfaceIAmAdmin()+"\n");
-           
-                
+
+            if (currentNode != null && currentSelection != null && args != null) {
+
+                System.out.print("selection: " + currentSelection + "node" + currentNode.getUserObject() + " \n");
+                System.out.print("admin: " + Interfaces.interfaceIAmAdmin() + "\n");
+
                 //datei erstellen
-                if (CFI_CMD.equals(command))
-                {
+                if (CFI_CMD.equals(command)) {
                     RightClickActions.FileCreate(currentNode, args);
-                //datei umbenennen
-                } else if (REFI_CMD.equals(command))
-                {
+                    //datei umbenennen
+                } else if (REFI_CMD.equals(command)) {
 
                     RightClickActions.FileRename(currentNode, args);
-                //datei löschen
-                } else if (RFI_CMD.equals(command))
-                {
+                    //datei löschen
+                } else if (RFI_CMD.equals(command)) {
                     RightClickActions.FileDelete(currentNode, args);
-                } 
-                //datei download
-                else if (DFI_CMD.equals(command) && currentNode != null)
-                {
+                } //datei download
+                else if (DFI_CMD.equals(command) && currentNode != null) {
                     RightClickActions.FileDownload(currentNode, args);
                 }
-                    
+
             }
-               
-                
-            
-            
-            
-            
 
         }
 
