aef50eceadd8c20fda807f6dca4911d1c03137c0
No0b7 <davidlampa7@gmail.com>
2014-10-20 14:12:56 +0200

    kackreiz nr 1

--- verteiltesFilesystem_origin/src/network/PingServer.java
+++ verteiltesFilesystem_origin/src/network/PingServer.java
@@ -16,6 +16,7 @@ import static java.lang.Thread.sleep;
 import java.net.SocketTimeoutException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.net.SocketFactory;
 
 /**
  * Klasse PingServer
@@ -57,54 +58,53 @@ public class PingServer
          */
         do
         {
-            try
-            {
-                /**
-                 * Starte den Socket mit Verbindung zum Gewünschten User 
-                 * mit einem Max. Timeout von einer Sekunde
-                 * Setze chk auf true
-                 */
-                SocketAddress sockaddr = new InetSocketAddress(checkIP, 1717);
-                socket.connect(sockaddr, 1000);
-                chk = true;
-            } catch (SocketTimeoutException ex)
-            {
                 try
                 {
                     /**
-                     * Warte eine Halbe Sekunde
+                     * Starte den Socket mit Verbindung zum Gewünschten User
+                     * mit einem Max. Timeout von einer Sekunde
+                     * Setze chk auf true
                      */
-                    sleep(800);
-                } catch (InterruptedException ex1)
+//                     SocketAddress sockaddr = new InetSocketAddress(checkIP, 1717);
+//                     socket.connect(sockaddr, 1000);
+                    Socket s = SocketFactory.getDefault().createSocket(checkIP, 1717);
+                    outMsg.print("AAAAAAAAAAAAAA " + s.isConnected(), 3);
+                    if(s.isConnected() == true)
+                    {
+                        chk = true;
+                    }
+                } catch (IOException ex)
                 {
-                    /**
-                     * Fehler abfangen, ausgeben - falls Sleep Fehlschlägt
-                     */
-                    outMsg.print("(PingServer) " + ex1.toString(), 1);
+                    outMsg.print("------------------------------------",3);
+                        counter++;
+                        try {
+                            sleep(800);
+                        } catch (InterruptedException ex1) {
+                            outMsg.print("(PingServer) " + ex1.toString(), 1);
+                        }
+                        chk = false;
+                    
+                        if (counter >= 4)
+                        {
+                            outMsg.print("IP: " + checkIP + " wurde aus dem Netzwerk entfernt, da nicht erreichbar!", 1);
+                            return false;
+                        }
                 }
-                
-                /**
-                 * Setze chk als false - nicht geklappt
-                 * Zähle den Counter hoch
-                 */
-                chk = false;
-                counter++;
-                ex.printStackTrace();
-                
-                /**
-                 * Soald die Verbindung zum gewünschten User 4 mal nicht erfolgreich war
-                 * Lösche die IP aus der Liste, Melde dass die IP gelöscht wurde
-                 * Gebe flase zurück // nicht Erfolgreich
-                 */
-                if (counter >= 8)
-                {
-                    outMsg.print("IP: " + checkIP + " wurde aus dem Netzwerk entfernt, da nicht erreichbar!", 1);
-                    return false;
-                }
-            } catch (IOException ex)
-            {
-                outMsg.print("(CheckWhoIsOnline - PingServer) : " + ex.toString(), 2);
-            }
+                try
+                    {
+                        /**
+                         * Schließe die Socket Verbindung
+                         */
+                        socket.close();
+                    } catch (IOException ex)
+                    {
+                        /**
+                         * Fehler abfangen, ausgeben und fehlgeschlagen zurückgeben
+                         */
+                        outMsg.print("(CheckWhoIsOnline - PingServer) : " + ex.toString(), 2);
+                        
+                    }
+            
         } while (!chk);
         try
         {
